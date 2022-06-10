df4e827c2f3151ad91a17396e4a82b389eeb6a48
KSSaiTeja <107067579+KSSaiTeja@users.noreply.github.com>
2022-06-10 07:42:43 +0530

    diamond pattern

---
+++ diamond.java
@@ -0,0 +1,37 @@
+package JavaPracticePrograms;
+
+public class Diamond {
+
+public static void main(String[] args) {
+
+for (int i = 0; i < 7; i++) {
+for (int j = 0; j <= 8 - i; j++) {
+System.out.print(" ");
+}
+
+for (int k = 0; k < 2 * i - 1; k++)
+
+{
+System.out.print("*");
+}
+
+System.out.println();
+
+}
+
+for (int i = 5; i > 0; i--) {
+System.out.print(" ");
+for (int j = 8 - i; j > 0; j--) {
+System.out.print(" ");
+}
+for (int k = 2 * i - 1; k > 0; k--) {
+System.out.print("*");
+}
+
+System.out.println();
+
+}
+
+}
+}
+            
